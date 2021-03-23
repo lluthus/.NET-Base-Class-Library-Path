6a316af8411ec167fde41b9defa7aa537f84afce
lluthus <lluthus@gmail.com>
2021-03-23 22:13:17 +0100

    start

---
+++ DataProcessor/FileProcessor.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+using System.IO;
+
+namespace DataProcessor
+{
+	internal class FileProcessor
+	{
+		public string InputFilePath;
+
+		public FileProcessor(string filePath)
+		{
+			InputFilePath = filePath;
+		}	
+
+		public void Process()
+		{
+			WriteLine($"Begin process of {InputFilePath}");
+
+			//if(File.Exists())
+		}
+
+	}
+}
